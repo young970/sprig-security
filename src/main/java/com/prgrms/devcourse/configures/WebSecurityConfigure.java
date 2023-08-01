bd0eeaf37dffbba163ec14a0e3864399844b7778
young970 <yyww9@naver.com>
2023-08-01 16:52:29 +0900

    feat: 1일차 미션 구현

--- src/main/java/com/prgrms/devcourse/configures/WebSecurityConfigure.java
+++ src/main/java/com/prgrms/devcourse/configures/WebSecurityConfigure.java
@@ -2,11 +2,18 @@ package com.prgrms.devcourse.configures;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
+@EnableWebSecurity
 public class WebSecurityConfigure {
 
   //스프링 시큐리티 필터 채인을 태우지 않겠다는 의미
@@ -24,9 +31,32 @@ public class WebSecurityConfigure {
                     .requestMatchers("/me").hasAnyRole("USER", "ADMIN")
                     .anyRequest().permitAll()
             )
-            .formLogin(login->login.defaultSuccessUrl("/").permitAll());
+            .formLogin(login->login.defaultSuccessUrl("/").permitAll())
+            .logout()
+            .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+            .logoutSuccessUrl("/")
+            .invalidateHttpSession(true)
+            .clearAuthentication(true)
+            .and()
+            .rememberMe()
+            .rememberMeParameter("remember-me")
+            .tokenValiditySeconds(300);
 
     return http.build();
 
+  }
+
+  @Bean
+  UserDetailsService userDetailsService(){
+    InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+    manager.createUser(User.withUsername("user")
+            .password("{noop}user123")
+            .roles("USER")
+            .build());
+    manager.createUser(User.withUsername("admin")
+            .password("{noop}admin123")
+            .roles("ADMIN")
+            .build());
+    return manager;
   }
 }
